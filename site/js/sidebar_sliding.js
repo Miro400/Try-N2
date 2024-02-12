25f25b8f63dc1224071fcda524a0e75ac06b51ac
Starwarik <81326689+Starwarik@users.noreply.github.com>
2024-02-12 01:10:38 +0000

    sidebar sliding ver. 0.2

--- site/js/sidebar_sliding.js
+++ site/js/sidebar_sliding.js
@@ -9,29 +9,30 @@ button.addEventListener("click", swap);
 // Swap sidebar state
 function swap(){
     let opposite = !isMovedOut;
+    console.log(opposite);
     toggle(opposite);
     isMovedOut = opposite;
 }
 
 function toggle(){
-    let sidebarwidth = sidebar.style.width;
-    let buttonpos = button.style.left;
+    let sidebarwidth = Number(sidebar.style.width.slice(0, -2));
+    let buttonpos = Number(button.style.left.slice(0, -2));
 
     switch(isMovedOut){
         case false:
             while (sidebarwidth < 200 & buttonpos < 200){
                 sidebarwidth += 2;
                 buttonpos += 2;
-                sidebar.style.width = sidebarwidth;
-                button.style.left = buttonpos;
+                sidebar.style.width = sidebarwidth + 'px';
+                button.style.left = buttonpos + 'px';
             }
             break;
         case true:
             while (sidebarwidth > 0 & buttonpos > 0){
                 sidebarwidth -= 2;
                 buttonpos -= 2;
-                sidebar.style.width = sidebarwidth;
-                button.style.left = buttonpos;
+                sidebar.style.width = sidebarwidth + 'px';
+                button.style.left = buttonpos + 'px';
             }
             break;
     }
