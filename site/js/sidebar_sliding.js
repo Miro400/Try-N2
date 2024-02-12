9a5c28ff789211e7461793997eed2af4de94785c
Starwarik <81326689+Starwarik@users.noreply.github.com>
2024-02-12 01:43:36 +0000

    sidebar sliding ver. 0.3

--- site/js/sidebar_sliding.js
+++ site/js/sidebar_sliding.js
@@ -1,6 +1,8 @@
 // Declare variables
 let button = document.querySelector(".sidebar");
 let sidebar = document.querySelector("sidebar");
+let buttonstyle = getComputedStyle(button);
+let sidebarstyle = getComputedStyle(sidebar);
 let isMovedOut = false;
 
 // Add button listener
@@ -15,24 +17,24 @@ function swap(){
 }
 
 function toggle(){
-    let sidebarwidth = Number(sidebar.style.width.slice(0, -2));
-    let buttonpos = Number(button.style.left.slice(0, -2));
+    let sidebarwidth = Number(sidebarstyle.getPropertyValue("width").slice(0, -2));
+    let buttonpos = Number(buttonstyle.getPropertyValue("left").slice(0, -2));
 
     switch(isMovedOut){
         case false:
             while (sidebarwidth < 200 & buttonpos < 200){
                 sidebarwidth += 2;
                 buttonpos += 2;
-                sidebar.style.width = sidebarwidth + 'px';
-                button.style.left = buttonpos + 'px';
+                sidebarstyle.setProperty("width", sidebarwidth + 'px');
+                buttonstyle.setProperty("left", buttonpos + 'px');
             }
             break;
         case true:
             while (sidebarwidth > 0 & buttonpos > 0){
                 sidebarwidth -= 2;
                 buttonpos -= 2;
-                sidebar.style.width = sidebarwidth + 'px';
-                button.style.left = buttonpos + 'px';
+                sidebarstyle.setProperty("width", sidebarwidth + 'px');
+                buttonstyle.setProperty("left", buttonpos + 'px');
             }
             break;
     }
