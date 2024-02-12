a10dbecf060bcaac240e59e9a69c6851c5047994
Starwarik <81326689+Starwarik@users.noreply.github.com>
2024-02-13 01:13:23 +0300

    sidebar sliding ver. 0.5

--- site/js/sidebar_sliding.js
+++ site/js/sidebar_sliding.js
@@ -1,32 +1,28 @@
 // Declare variables
+let button = document.querySelector(".sidebar");
+let sidebar = document.querySelector("sidebar");
+let isMovedOut = false;
 
-document.addEventListener("load", (event) => {
-    let button = document.querySelector(".sidebar");
-    let sidebar = document.querySelector("sidebar");
-    let isMovedOut = false;
+// Add button listener
+button.addEventListener("click", swap);
 
-    // Add button listener
-    button.addEventListener("click", swap);
+// Swap sidebar state
+function swap(){
+    let opposite = !isMovedOut;
+    console.log(opposite);
+    toggle(opposite);
+    isMovedOut = opposite;
+}
 
-    // Swap sidebar state
-    function swap(){
-        let opposite = !isMovedOut;
-        console.log(opposite);
-        toggle(opposite);
-        isMovedOut = opposite;
+function toggle(){
+    switch(isMovedOut){
+        case false:
+            sidebar.style.setProperty("width", "0px");
+            button.style.setProperty("left", "0px");
+            break;
+        case true:
+            sidebar.style.setProperty("width", "200px");
+            button.style.setProperty("left", "200px");
+            break;
     }
-
-    function toggle(){
-        switch(isMovedOut){
-            case false:
-                sidebar.style.setProperty("width", "0px");
-                button.style.setProperty("left", "0px");
-                break;
-            case true:
-                sidebar.style.setProperty("width", "200px");
-                button.style.setProperty("left", "200px");
-                break;
-        }
-    }
-});
-
+}
