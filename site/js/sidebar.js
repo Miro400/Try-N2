a7298f8136074e26a269ef39bde890849efe1089
Starwarik <81326689+Starwarik@users.noreply.github.com>
2024-02-13 15:35:18 +0100

    Update and rename site/js/sidebar.js to site/include_html.js

--- site/js/sidebar.js
+++
@@ -1,9 +0,0 @@
-fetch("sidebar.html") 
-.then((response) => response.text())
-.then(replace_sidebar(data)).catch((err) => console.log(err));
-
-function replace_sidebar(data) {
-  let old_element = document.querySelector("script#sidebar_replace");
-  let new_element = new DOMParser().parseFromString(data, "text/html").querySelectorAll("#sidebar");
-  old_element.parentNode.replaceChild(new_element, old_element);
-}
