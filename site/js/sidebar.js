59c9ecb5b88537573789bf9f87f7d3903ac63636
Starwarik <81326689+Starwarik@users.noreply.github.com>
2024-02-13 15:03:46 +0100

    Update sidebar.js

--- site/js/sidebar.js
+++ site/js/sidebar.js
@@ -1,6 +1,9 @@
 fetch("sidebar.html") 
 .then((response) => response.text())
-.then((data) => { let old_element = document.querySelector("script#sidebar_replace");
-                   let new_element = new DOMParser().parseFromString(data, "text/html").querySelector("sidebar");
-                   old_element.parentNode.replaceChild(new_element, old_element);
-                  }).catch((err) => console.log(err));
+.then(replace_sidebar(data)).catch((err) => console.log(err));
+
+function replace_sidebar(data) {
+  let old_element = document.querySelector("script#sidebar_replace");
+  let new_element = new DOMParser().parseFromString(data, "text/html").querySelectorAll("#sidebar");
+  old_element.parentNode.replaceChild(new_element, old_element);
+}
