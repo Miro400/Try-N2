69e9a073635e3f5c229d510c1afc6bbfd4cb4b59
Starwarik <81326689+Starwarik@users.noreply.github.com>
2024-02-15 11:26:36 +0100

    Update topbar.js

--- site/js/topbar.js
+++ site/js/topbar.js
@@ -1,7 +1,9 @@
 fetch("topbar.html") 
 .then((response) => response.text())
 .then((data) => { let old_element = document.querySelector("script#topbar_replace");
-                  let new_element = new DOMParser().parseFromString(data, "text/html");//.querySelector("topbar");
+                  let new_doc = new DOMParser().parseFromString(data, "text/html");
+                 console.log(new_doc);
+                 let new_element = new_doc.querySelector("topbar");
                   console.log(new_element);
-                   //old_element.parentNode.replaceChild(new_element, old_element);
+                  old_element.parentNode.replaceChild(new_element, old_element);
                   }).catch((err) => console.log(err));
